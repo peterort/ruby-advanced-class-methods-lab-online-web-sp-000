eaf69ba981a2c64cd913efa6b91aded3374d19b4
Peter Ort <peter@peterort.com>
2020-06-01 17:16:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,64 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+      song = self.new
+      song.save
+      song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(search)
+    self.all.find {|s| s.name == search}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      return self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|s| s.name}
+  end
+
+  def self.new_from_filename(filename)
+    array = filename.split (" - ")
+    artist = array[0]
+    song_name = array[1].chomp(".mp3")
+    song = self.create
+    song.artist_name = artist
+    song.name = song_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    array = filename.split (" - ")
+    artist = array[0]
+    song_name = array[1].chomp(".mp3")
+    song = self.create
+    song.artist_name = artist
+    song.name = song_name
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
